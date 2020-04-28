38a26edfc9dd923243da204b41851a6d1a9b38ee
takeokunn <bararararatty@gmail.com>
2020-04-28 11:40:38 +0900

    First commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Controller2D controller;
+
+    void Start()
+    {
+
+    }
+}
